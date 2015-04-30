69a2ec8bd2c82f857b1a823428021d80d5b75275
yocoHM <yocoyanihernandez@gmail.com>
2015-04-30 09:16:35 -0500

    ya esta el usuario admin

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,7 @@ class RecipesController < ApplicationController
 	before_action :set_recipe, only: [:edit, :update, :show, :like]
 	before_action :require_user, except: [:show, :index]
 	before_action :require_same_user, only: [:edit, :update]
+	before_action :admin_user, only: :destroy
 
 	def index
 		@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
@@ -56,6 +57,12 @@ class RecipesController < ApplicationController
 		end
 	end
 
+	def destroy
+		Recipe.find(params[:id]).destroy
+		flash[:success] = "Recipe deleted"
+		redirect_to recipes_path
+	end
+
 	#actions privadas
 	private
 
@@ -68,10 +75,14 @@ class RecipesController < ApplicationController
 			end
 
 			def require_same_user
-				if current_user != @recipe.chef
+				if current_user != @recipe.chef and !current_user.admin?
 					flash[:danger] = "You can only edit your own recipes"
 					redirect_to recipes_path
 				end
-		end
+			end
+
+			def admin_user
+				redirect_to recipes_path unless current_user.admin?
+			end
 
 end
