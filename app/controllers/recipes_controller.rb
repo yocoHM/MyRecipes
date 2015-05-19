2f8b23940799fd33529c2b6c586cd1857290986a
yocoHM <yocoyanihernandez@gmail.com>
2015-05-18 20:18:07 -0500

    checkboxes para relacionar los ingredientes y estilos con una receta

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -68,7 +68,7 @@ class RecipesController < ApplicationController
 	private
 
 			def recipe_params
-				params.require(:recipe).permit(:name, :summary, :description)
+				params.require(:recipe).permit(:name, :summary, :description, style_ids: [], ingredient_ids: [])
 			end
 
 			def set_recipe
